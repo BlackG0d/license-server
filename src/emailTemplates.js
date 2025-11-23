99a34961351ef46393f8640414753c17df4194e5
BlackG0d <75131860+BlackG0d@users.noreply.github.com>
2025-11-23 12:08:45 +0500

    Increase height of table cell from 10 to 20

--- src/emailTemplates.js
+++ src/emailTemplates.js
@@ -104,7 +104,7 @@ function getVerificationEmailHtml(code) {
   margin-bottom:16px;
 ">
 <tr>
-    <td height="10"></td>
+    <td height="20"></td>
   </tr>
   <tr>
     <td align="center" valign="middle">
